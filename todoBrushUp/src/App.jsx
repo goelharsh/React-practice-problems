a9f8b2c40e46aedbdbb9cc7ed9d6efd949368a89
napharsh <harsh.goel@napworks.in>
2024-08-12 22:23:04 +0530

    todoBrushup

---
+++ todoBrushUp/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ 
+  const [inputText, setInputText] = useState("");
+  const [todoList, setTodoList] = useState([]);
+  function changeHandler(e){
+    setInputText(e.target.value);
+  }
+  const handleAdd = ()=>{
+    setTodoList([...todoList,inputText]);
+    setInputText("")
+  }
+  const handleDelete = (id)=>{
+    setTodoList(todoList.filter((todo,index)=>index !== id));
+  }
+  return (
+   <div>
+    <input type='text' value={inputText} onChange={changeHandler}/>
+    <button onClick={handleAdd}>Add</button>
+   
+
+    <ul>
+      {
+        todoList.map((todo,index)=>(
+          <>
+           <li key={index}>{todo}</li>
+           <button onClick={handleDelete(index)}>Delete</button></>
+        ))
+      }
+    </ul>
+   </div>
+  )
+}
+
+export default App
