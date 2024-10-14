f298fe71cad1fe28ff0d6a82a88ccd65121c51a6
napharsh <harshgoel18122001@gmail.com>
2024-10-14 21:47:57 +0530

    React hook form

---
+++ reacthookform/src/App.jsx
@@ -0,0 +1,18 @@
+import { useForm } from "react-hook-form";
+
+
+function App() {
+ 
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input defaultValue="test" {...register("example")} />
+      <input {...register("exampleRequired", { required: true })} />
+      {errors.exampleRequired && <span>This field is required</span>}
+      <input type="submit" />
+    </form>
+  )
+}
+
+export default App
